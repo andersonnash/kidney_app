d267196e08cd12a1545dd9df2d3b8ca3617576f2
Nash Anderson <nashanderson@Nashs-MacBook-Air.local>
2021-07-15 06:40:24 -0600

    style tweaks

--- client/src/components/Homepage.jsx
+++ client/src/components/Homepage.jsx
@@ -4,13 +4,19 @@ import { Link } from "react-router-dom";
 export default function Homepage() {
   return (
     <div>
-      <div className="homepageLinks">
-        <Link to="/Transplant">Transplant</Link>
+      {/* <div className="homepageImages"> */}
+      <Link to="/Transplant">
+        <img src="https://www.kidney.org/sites/default/files/styles/nephron_custom_scale_crop_300_225/public/transplant_friends.jpg" />
+      </Link>
 
-        <Link to="/Forum">Forum</Link>
+      <Link to="/Forum">
+        <img src="https://www.codlearningtech.org/wp-content/uploads/2020/07/discussionboard-1.png" />
+      </Link>
 
-        <Link to="/Iga">Living with Iga Nephropathy</Link>
-      </div>
+      <Link to="/Iga">
+        <img src="http://cdn.shopify.com/s/files/1/0379/5923/7764/files/body-fail-kidney-man-organs-people-sad-7EtK4d-clipart_large.jpeg?v=1595044917" />
+      </Link>
+      {/* </div> */}
     </div>
   );
 }
